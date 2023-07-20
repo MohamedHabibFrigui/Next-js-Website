d75f82082777a0f4f900241ec6b0527d8dce8d22
Mohamed Habib Frigui <mohamedhabibfrigui9@gmail.com>
2023-07-20 15:10:32 +0100

    Add Portfolio and Blog pages

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,7 +1,33 @@
+import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 const Blog = () => {
-  return <div className={styles.container}>Blog</div>;
+  return (
+    <div className={styles.mainContainer}>
+      {data.map((item) => (
+        <Link
+          href={`/blog/${item._id}`}
+          className={styles.container}
+          key={item.id}
+        >
+          <div className={styles.imageContainer}>
+            <Image
+              src={item.img}
+              alt=""
+              width={400}
+              height={250}
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.desc}</p>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default Blog;
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,7 +1,24 @@
 import styles from "./page.module.css";
 
-const Category = () => {
-  return <div className={styles.container}>Category</div>;
+const Category = ({ params }) => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+
+      {data.map((item) => (
+        <div className={styles.item} key={item.id}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.desc}</p>
+            <Button text="See More" url="#" />
+          </div>
+          <div className={styles.imgContainer}>
+            <Image className={styles.img} fill={true} src={item.image} alt="" />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Category;
