6dfa4c3d7c2d4e5d7667424185db19ab3ce4a381
Mohamed Habib Frigui <mohamedhabibfrigui9@gmail.com>
2023-07-20 10:36:28 +0100

    Add the Navbar and the Footer, and the css files skeletons

---
+++ src/component/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.css";
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <div>&copy;2023 Lamania. All rights reserved.</div>
+    </div>
+  );
+};
+
+export default Footer;
