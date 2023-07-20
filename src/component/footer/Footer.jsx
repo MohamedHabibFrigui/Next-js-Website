369b2308bc340a35279f2390daba8c18f628365d
Mohamed Habib Frigui <mohamedhabibfrigui9@gmail.com>
2023-07-20 14:37:23 +0100

    Add Home and About pages and Button component

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -1,9 +1,40 @@
-import styles from "./page.module.css";
+import Image from "next/image";
+import styles from "./footer.module.css";
 
 const Footer = () => {
   return (
     <div className={styles.container}>
       <div>&copy;2023 Lamania. All rights reserved.</div>
+      <div className={styles.social}>
+        <Image
+          className={styles.icon}
+          src="/1.png"
+          width={15}
+          height={15}
+          alt="Facebook"
+        />
+        <Image
+          className={styles.icon}
+          src="/2.png"
+          width={15}
+          height={15}
+          alt="Instagram"
+        />
+        <Image
+          className={styles.icon}
+          src="/3.png"
+          width={15}
+          height={15}
+          alt="Twitter"
+        />
+        <Image
+          className={styles.icon}
+          src="/4.png"
+          width={15}
+          height={15}
+          alt="Youtube"
+        />
+      </div>
     </div>
   );
 };
